d38e26600303edef743e75b8ec6610da6ad883e1
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-12 22:22:22 +0100

    Add user_id to friend controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -64,6 +64,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :user_id)
     end
 end
