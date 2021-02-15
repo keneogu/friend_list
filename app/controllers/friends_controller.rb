ea4f0d3b723d9b3693cdef2a8066361643e7e6a8
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-02-15 22:03:49 +0100

    Only shows the friend list of a prticular friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,8 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
@@ -12,7 +14,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,8 +24,9 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
-
+    #@friend = Friend.new(friend_params)
+     @friend = current_user.friends.build(friend_params )
+    
     respond_to do |format|
       if @friend.save
         format.html { redirect_to @friend, notice: "Friend was successfully created." }
@@ -54,6 +58,11 @@ class FriendsController < ApplicationController
       format.html { redirect_to friends_url, notice: "Friend was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not authorized to Edit this friend" if @friend.nil?
   end
 
   private
